51413b333bf9dd93bb2faf1e498accacf7bbb131
Kukki777 <priyansh23042004@gmail.com>
2025-06-11 17:26:40 +0530

    after model

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import express from "express";
+const app = express();
+
 
 dotenv.config({
     path:'./env'
@@ -10,6 +13,15 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is Listening on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit the process with failure
+});
 
 
 // import express from "express";
